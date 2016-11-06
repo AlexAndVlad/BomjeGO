beafda16b1842135e0e5f9fa361359bc3126c174
Vlad <vlad082006@yandex.ru>
2016-11-06 20:25:28 +0300

    test

--- app/src/main/java/alexandvlad/bomjego/MapsActivity.java
+++ app/src/main/java/alexandvlad/bomjego/MapsActivity.java
@@ -4,6 +4,7 @@ import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -47,6 +48,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
+            Log.wtf("ASSERT", "GOT NO PERMISSION!");
             return;
         }
         mMap.setMyLocationEnabled(true);
